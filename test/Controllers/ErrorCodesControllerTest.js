4fd7372d6664f1c35aa7b36ec8ff20d3d0f70c4b
Maham Shahid <maham.shahid@apimatic.io>
2017-03-14 11:47:33 +0500

    made some changes

--- test/Controllers/ErrorCodesControllerTest.js
+++ test/Controllers/ErrorCodesControllerTest.js
@@ -4,7 +4,6 @@
  * This file was automatically generated for Stamplay by APIMATIC v2.0 ( https://apimatic.io )
  */
 var assert = require("chai").assert;
-var moment = require("moment");
 var TestHelper = require("../TestHelper");
 var APIHelper = require("../../lib/APIHelper");
 var testerlib = require("../../lib");
@@ -12,16 +11,6 @@ var config = require("../../lib/configuration");
 var testConfiguration = require("../TestBootstrap");
 
 var controller = testerlib.ErrorCodesController;
-var Person = testerlib.Person;
-var ServerResponse = testerlib.ServerResponse;
-var EchoResponse = testerlib.EchoResponse;
-var QueryParameter = testerlib.QueryParameter;
-var Days = testerlib.Days;
-var SuiteCode = testerlib.SuiteCode;
-var Employee = testerlib.Employee;
-var GlobalTestException = testerlib.GlobalTestException;
-var LocalTestException = testerlib.LocalTestException;
-var Boss = testerlib.Boss;
 
 
 describe("ErrorCodesController Tests", function tests() {
